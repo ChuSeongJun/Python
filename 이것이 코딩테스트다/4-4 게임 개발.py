b78cc8b439f8ab9ead3645fb264b621e5c2afdbb
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-06-03 20:30:08 +0900

    진짜 너무어려워

--- 이것이 코딩테스트다/4-4 게임 개발.py
+++ 이것이 코딩테스트다/4-4 게임 개발.py
@@ -1,55 +1,57 @@
 
-
 n, m = map(int, input().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 X 좌표, Y 좌표, 방향을 입력받기
 a, b, direction = map(int, input().split())
+d[a][b] = 1 # 현재 좌표 방문 처리
 
-d = [[0]*m for _ in range(n)]
-# 방문한 위치 저장하기 위한 맵 생성해 0으로 초기화
-d[a][b] = 1
-# 현재 좌표 방문 처리
-
-# 0 북쪽 1 동쪽 2 남쪽 3 서쪽
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
+# 전체 맵 정보를 입력받기
 array = []
 for i in range(n):
     array.append(list(map(int, input().split())))
 
-result = 0
-turn = 0
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-
+# 왼쪽으로 회전
 def turn_left():
     global direction
     direction -= 1
-    if direction == 0:
+    if direction == -1:
         direction = 3
 
 
-while 1:
+count = 1
+turn_time = 0
+while True:
     turn_left()
-    na = a+dx[direction]
-    nb = b+dx[direction]
-    if d[na][nb] == 0 and array[na][nb] == 0:
-        d[na][nb] = 1
-        a = na
-        b = nb
-        result += 1
-        turn = 0
+    nx = a + dx[direction]
+    ny = b + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        a = nx
+        b = ny
+        count += 1
+        turn_time = 0
         continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
     else:
-        turn += 1
-
-    if turn == 4:
-        na = a-dx[direction]
-        nb = b-dy[direction]
-        if array[na][nb] == 0:
-            a = na
-            b = nb
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = a - dx[direction]
+        ny = b - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            a = nx
+            b = ny
+        # 뒤가 바다로 막혀있는 경우
         else:
             break
-        turn = 0
+        turn_time = 0
 
-print(result)
+print(count)
